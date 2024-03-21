39b6668e96b04a9354909b1ce7b8869d47e027f7
andreglock <andreglock@gmail.com>
2024-03-21 20:44:27 +0100

    third exercise

--- src/pages/exercises/ssr-exercise.tsx
+++ src/pages/exercises/ssr-exercise.tsx
@@ -2,7 +2,13 @@ import Character from "@/components/Character";
 import { ExerciseTitle } from "@/components/ExerciseTitle";
 import { PageWrapper } from "@/components/PageWrapper";
 import { RickAndMortyCharacterResponse } from "@/models/APITypes";
-import { useQuery } from "@tanstack/react-query";
+import {
+  DehydratedState,
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+  useQuery,
+} from "@tanstack/react-query";
 import React from "react";
 
 async function getServerSideMorties() {
@@ -12,7 +18,11 @@ async function getServerSideMorties() {
   return response;
 }
 
-function SSRExercise() {
+function SSRExercise({
+  dehydratedState,
+}: {
+  dehydratedState: DehydratedState;
+}) {
   const { data } = useQuery({
     queryKey: ["server-side-morties"],
     queryFn: getServerSideMorties,
@@ -22,14 +32,16 @@ function SSRExercise() {
   // when loading the page, you shouldn't see a network request
   // what problems can you identify regarding the query key?
   return (
-    <PageWrapper>
-      <ExerciseTitle title="Server Side Rendering Exercise"></ExerciseTitle>
-      <div className="grid grid-columns-cards gap-4 pt-4">
-        {data?.results?.map((character) => (
-          <Character key={character.id} character={character}></Character>
-        ))}
-      </div>
-    </PageWrapper>
+    <HydrationBoundary state={dehydratedState}>
+      <PageWrapper>
+        <ExerciseTitle title="Server Side Rendering Exercise"></ExerciseTitle>
+        <div className="grid grid-columns-cards gap-4 pt-4">
+          {data?.results?.map((character) => (
+            <Character key={character.id} character={character}></Character>
+          ))}
+        </div>
+      </PageWrapper>
+    </HydrationBoundary>
   );
 }
 
@@ -37,5 +49,11 @@ export default SSRExercise;
 
 export const getServerSideProps = async () => {
   // prefetch the data here, on the server side
-  return { props: {} };
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["server-side-morties"],
+    queryFn: getServerSideMorties,
+  });
+  const serverSideData = await getServerSideMorties();
+  return { props: { dehydratedState: dehydrate(queryClient) } };
 };
